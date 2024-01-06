ed7a4f7a95daa62f7dc21d64325b0a060e040ad6
nittish03 <nittishbaboria03@gmail.com>
2024-01-07 01:19:43 +0530

    made new changes in the code and added the buttons for animation

---
+++ index.js
@@ -0,0 +1,19 @@
+let r=document.getElementById("re");
+let g=document.getElementById("gr");
+let y=document.getElementById("ye");
+let a=document.getElementById("start");
+
+
+    a.onclick=()=>{
+        r.style.animation="animation3 2s 1s 1 normal none";
+        g.style.animation=" animation1 5s 5s 1 normal none";
+        y.style.animation="animation2 2s 3s 1 normal none";
+        }
+
+let b=document.getElementById("end");
+        b.onclick=()=>{
+        r.style.animation="animation3 5s 5s 1 normal none";
+        g.style.animation="animation1 2s 1s  1 normal none";
+        y.style.animation="animation2 2s  3s 1 normal none";      
+        }
+
