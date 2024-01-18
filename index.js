fff48447650b4486795e1cc19fde00382067bb8d
nittish03 <nittishbaboria03@gmail.com>
2024-01-18 22:53:24 +0530

    added the reset button. damn took me a lot of time

--- index.js
+++ index.js
@@ -2,18 +2,49 @@ let r=document.getElementById("re");
 let g=document.getElementById("gr");
 let y=document.getElementById("ye");
 let a=document.getElementById("start");
+let b=document.getElementById("end");
+let d=document.getElementById("container");
 
+let cb=document.createElement("button");
+cb.setAttribute("id","reset");
+cb.innerText=("RESET");
 
-    a.onclick=()=>{
+// let e=document.createElement("button")
+// e.setAttribute("id","end");
+// e.setAttribute("class","but");
+// e.innerText=("GREEN")
+
+// let f=document.createElement("button")
+// f.setAttribute("id","start");
+// f.setAttribute("class","but");
+// f.innerText=("RED")
+
+a.onclick=()=>{
         r.style.animation="animation3 2s 1s 1 normal none";
         g.style.animation=" animation1 5s 5s 1 normal none";
         y.style.animation="animation2 2s 3s 1 normal none";
+        b.after(cb)
+        a.remove();
+        b.remove();
         }
-
-let b=document.getElementById("end");
-        b.onclick=()=>{
+b.onclick=()=>{
         r.style.animation="animation3 5s 5s 1 normal none";
         g.style.animation="animation1 2s 1s  1 normal none";
-        y.style.animation="animation2 2s  3s 1 normal none";      
+        y.style.animation="animation2 2s  3s 1 normal none";
+        b.after(cb);
+        a.remove();
+        b.remove();  
         }
+cb.onclick=()=>{
+    document.location.reload();
+    r.style.animation="";
+    g.style.animation="";
+    y.style.animation="";
+    // cb.remove();
+    // d.after(f);
+    // d.after(e);
+}
+
+
+
 
